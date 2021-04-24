c2b71b336e5e85c221dcc953f0fb2f63afe07937
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-24 16:23:03 +0900

    test : PlayerTest 추가

---
+++ src/test/java/domain/user/PlayerTest.java
@@ -0,0 +1,31 @@
+package domain.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @DisplayName("Player 생성을 테스트한다.")
+    @Test
+    void createPlayerTest() {
+        // given
+        PlayerName playerName = new PlayerName("player");
+        BettingMoney bettingMoney = new BettingMoney(new BigDecimal(1000));
+
+        // when
+        Player player = new Player(playerName, bettingMoney);
+
+        // then
+        assertAll(
+                () -> assertThat(player).isInstanceOf(Player.class),
+                () -> assertThat(player.getBettingMoney()).isEqualTo(bettingMoney),
+                () -> assertThat(player.getName()).isEqualTo(playerName)
+        );
+    }
+
+}
