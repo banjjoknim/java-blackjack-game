cdf2c3d10bd464a879645e9c9bfc611b25fd810a
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-24 10:30:33 +0900

    feat : 기본으로 제공되는 BlackJackGame, Card, Dealer, Player 클래스 추가

---
+++ src/main/java/domain/user/Dealer.java
@@ -0,0 +1,19 @@
+package domain.user;
+
+import domain.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public Dealer() {
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+}
---
+++ src/main/java/domain/user/Player.java
@@ -0,0 +1,23 @@
+package domain.user;
+
+import domain.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    
+    private final String name;
+    private final double bettingMoney;
+    private final List<Card> cards = new ArrayList<>();
+
+    public Player(String name, double bettingMoney) {
+        this.name = name;
+        this.bettingMoney = bettingMoney;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+}
