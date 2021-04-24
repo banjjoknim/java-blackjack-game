11669409962521890e1b28c0552c152a55c94cf2
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-24 14:17:01 +0900

    feat : 플레이어의 배팅 금액 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.user.PlayerName;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -14,6 +16,11 @@ public class InputView {
         OutputView.pleaseInputNames();
         return Arrays.stream(scanner.nextLine().split(","))
                 .collect(toList());
+    }
+
+    public static double inputBettingMoneyAmount(PlayerName playerName) {
+        OutputView.askHowMuchBettingToPlayer(playerName);
+        return scanner.nextDouble();
     }
 
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,15 @@
 package view;
 
+import domain.user.PlayerName;
+
 public class OutputView {
 
     public static void pleaseInputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+    }
+
+    public static void askHowMuchBettingToPlayer(PlayerName playerName) {
+        System.out.println(playerName.getName() + "의 배팅 금액은?");
     }
 
 }
