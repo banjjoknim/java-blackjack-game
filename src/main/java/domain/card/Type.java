9ad9847e9bd07c913cf15c710e0c31ad4159557c
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-24 16:50:37 +0900

    refactor : Type enum의 분리된 ACE -> ACE 하나로 변경

--- src/main/java/domain/card/Type.java
+++ src/main/java/domain/card/Type.java
@@ -1,8 +1,19 @@
 package domain.card;
 
 public enum Type {
-    KING(10), QUEEN(10), JACK(10), TEN(10), NINE(9), EIGHT(8), SEVEN(7),
-    SIX(6), FIVE(5), FOUR(4), THREE(3), TWO(2), ACE_ONE(1), ACE_ELEVEN(11);
+    KING(10),
+    QUEEN(10),
+    JACK(10),
+    TEN(10),
+    NINE(9),
+    EIGHT(8),
+    SEVEN(7),
+    SIX(6),
+    FIVE(5),
+    FOUR(4),
+    THREE(3),
+    TWO(2),
+    ACE(1);
 
     private int number;
 
