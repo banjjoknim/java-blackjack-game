dfc14b41936c3766b90622826c6e7747e5d8a73c
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-24 14:35:17 +0900

    refactor : 배팅 금액 입력시 double -> BigDecimal 로 입력되도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,6 +2,7 @@ package view;
 
 import domain.user.PlayerName;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,18 +10,18 @@ import java.util.Scanner;
 import static java.util.stream.Collectors.toList;
 
 public class InputView {
+    private static final String SEPARATOR = ",";
 
     private static Scanner scanner = new Scanner(System.in);
 
     public static List<String> inputNames() {
         OutputView.pleaseInputNames();
-        return Arrays.stream(scanner.nextLine().split(","))
+        return Arrays.stream(scanner.nextLine().split(SEPARATOR))
                 .collect(toList());
     }
 
-    public static double inputBettingMoneyAmount(PlayerName playerName) {
-        OutputView.askHowMuchBettingToPlayer(playerName);
-        return scanner.nextDouble();
+    public static BigDecimal inputBettingMoneyAmount() {
+        return new BigDecimal(scanner.next());
     }
 
 }
