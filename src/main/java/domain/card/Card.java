855a306cd0fa513d11e0ff5d6b721576b9a1bd75
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-24 10:30:33 +0900

    feat : 기본으로 제공되는 BlackJackGame, Card, Dealer, Player 클래스 추가

---
+++ src/main/java/domain/card/Card.java
@@ -0,0 +1,13 @@
+package domain.card;
+
+public class Card {
+
+    private final Symbol symbol;
+    private final Type type;
+
+    public Card(Symbol symbol, Type type) {
+        this.symbol = symbol;
+        this.type = type;
+    }
+
+}
