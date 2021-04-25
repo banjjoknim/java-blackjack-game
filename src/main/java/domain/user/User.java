5360d2fed648d8086823c4155907437742210128
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-25 22:29:52 +0900

    refactor : 유저의 카드 숫자합 계산 기능에 ACE가 포함되어 블랙잭이 되는 경우 고려하도록 변경

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -7,8 +7,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class User {
-    private static final int BLACK_JACK = 21;
+    private static final int INITIAL_SUM_OF_CARD_NUMBERS = 0;
     private static final int INITIAL_CARDS_SIZE = 2;
+    private static final int TEN = 10;
+    private static final int ACE_AND_TEN = 11;
+    private static final int BLACK_JACK = 21;
 
     private final List<Card> cards = new ArrayList<>();
 
@@ -25,9 +28,14 @@ public class User {
     }
 
     public int calculateSumOfCardNumbers() {
-        return cards.stream()
-                .mapToInt(card -> card.getType().getNumber())
-                .sum();
+        int sumOfCardNumbers = INITIAL_SUM_OF_CARD_NUMBERS;
+        for (Card card : cards) {
+            sumOfCardNumbers = sumOfCardNumbers + card.getType().getNumber();
+        }
+        if (cards.size() == INITIAL_CARDS_SIZE && sumOfCardNumbers == ACE_AND_TEN) {
+            sumOfCardNumbers = sumOfCardNumbers + TEN;
+        }
+        return sumOfCardNumbers;
     }
 
     public List<Card> getCards() {
