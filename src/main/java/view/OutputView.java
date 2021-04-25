5561e4bb1b69af7f3c18a2ec851133348cbda5c1
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-25 17:10:23 +0900

    feat : 딜러의 보유 카드 숫자 합이 16이하일 경우 메세지 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -47,4 +47,8 @@ public class OutputView {
         System.out.println(player.getPlayerName().getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
     }
 
+    public static void printDealerGetOneMoreCard() {
+        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+    }
+
 }
