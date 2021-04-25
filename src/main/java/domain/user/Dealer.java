62ed22d062112df68d18f117eae5bb78327ce43d
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-25 22:55:27 +0900

    feat : 딜러가 가진 카드 숫자 합이 규칙에 명시된 숫자보다 작은지 검증하는 기능 추가

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,8 +1,13 @@
 package domain.user;
 
-public class Dealer extends User{
+public class Dealer extends User {
+    private static final int DEALER_RULE_NUMBER = 17;
 
     public Dealer() {
+    }
+
+    public boolean hasSmallNumberLessThanRuleNumber() {
+        return super.calculateSumOfCardNumbers() < DEALER_RULE_NUMBER;
     }
 
 }
