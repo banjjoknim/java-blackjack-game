9e5139b30a4b24cd671f6e16b513536c170b05d3
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-26 01:35:42 +0900

    refactor : GameResult enum 클래스에 필드 추가 및 입력값에 따른 게임 결과 반환 기능 변경

--- src/main/java/domain/result/GameResult.java
+++ src/main/java/domain/result/GameResult.java
@@ -3,25 +3,39 @@ package domain.result;
 import java.util.Arrays;
 
 public enum GameResult {
-    WIN(1),
-    DRAW(0),
-    LOSE(-1);
+    WIN(1, false, 2),
+    WIN_WITH_BLACK_JACK(1, true, 2.5),
+    DRAW(0, false, 1),
+    DRAW_WITH_BLACK_JACK(0, true, 1),
+    LOSE(-1, false, -1);
 
     private int value;
+    private boolean isBlackJack;
+    private double dividendRate;
 
-    GameResult(int value) {
+    GameResult(int value, boolean isBlackJack, double dividendRate) {
         this.value = value;
+        this.isBlackJack = isBlackJack;
+        this.dividendRate = dividendRate;
     }
 
-    public static GameResult getGameResult(int matchResultValue) {
+    public static GameResult getGameResult(int gameResultValue, boolean isBlackJack) {
         return Arrays.stream(GameResult.values())
-                .filter(matchResult -> matchResult.value == matchResultValue)
+                .filter(gameResult -> gameResult.value == gameResultValue && gameResult.isBlackJack == isBlackJack)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("게임 결과는 승, 무, 패 중에서 하나여야 합니다."));
     }
 
     public int getValue() {
         return value;
+    }
+
+    public boolean isBlackJack() {
+        return isBlackJack;
+    }
+
+    public double getDividendRate() {
+        return dividendRate;
     }
 
 }
