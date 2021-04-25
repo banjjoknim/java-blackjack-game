a8af353071e4b2c0c96047cde6fe825dcdd24051
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-25 20:36:36 +0900

    test : UserTest 추가

---
+++ src/test/java/domain/user/UserTest.java
@@ -0,0 +1,72 @@
+package domain.user;
+
+import domain.card.Card;
+import domain.card.Symbol;
+import domain.card.Type;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @DisplayName("유저의 카드 추가 기능을 테스트한다.")
+    @Test
+    void addCardTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.addCard(new Card(Symbol.DIAMOND, Type.ACE));
+
+        // then
+        assertThat(user.getCards()).hasSize(1);
+    }
+
+    @DisplayName("유저의 카드 합 계산 기능을 테스트한다.")
+    @Test
+    void calculateSumOfCardNumbersTest() {
+        // given
+        User user = new User();
+        user.addCard(new Card(Symbol.SPADE, Type.KING));
+        user.addCard(new Card(Symbol.HEART, Type.KING));
+        user.addCard(new Card(Symbol.CLUB, Type.KING));
+
+        // when
+        int sumOfCardNumbers = user.calculateSumOfCardNumbers();
+
+        // then
+        assertThat(sumOfCardNumbers).isEqualTo(30);
+
+    }
+
+    @DisplayName("유저의 카드 합이 21이하일 경우 Bust 되었는지 판단하는 기능을 테스트한다.")
+    @Test
+    void determineIsBustWhenSumLessThanBlackJackTest() {
+        // given
+        User user = new User();
+
+        // when
+        boolean isBust = user.determineIsBust();
+
+        // then
+        assertThat(isBust).isFalse();
+    }
+
+    @DisplayName("유저의 카드 합이 21을 초과할 경우 Bust 되었는지 판단하는 기능을 테스트한다.")
+    @Test
+    void determineIsBustWhenSumExceededBlackJackTest() {
+        // given
+        User user = new User();
+        user.addCard(new Card(Symbol.SPADE, Type.KING));
+        user.addCard(new Card(Symbol.HEART, Type.KING));
+        user.addCard(new Card(Symbol.CLUB, Type.KING));
+
+        // when
+        boolean isBust = user.determineIsBust();
+
+        // then
+        assertThat(isBust).isTrue();
+    }
+
+}
