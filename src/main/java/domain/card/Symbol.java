43dcc1e2ddad416b8fb5431d802f2cdd2a050ceb
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-25 15:35:43 +0900

    refactor : Symbol enum 클래스 필드 추가

--- src/main/java/domain/card/Symbol.java
+++ src/main/java/domain/card/Symbol.java
@@ -1,5 +1,19 @@
 package domain.card;
 
 public enum Symbol {
-    SPADE, HEART, DIAMOND, CLUB
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드"),
+    CLUB("클로버");
+
+    private String symbolName;
+
+    Symbol(String symbolName) {
+        this.symbolName = symbolName;
+    }
+
+    public String getSymbolName() {
+        return symbolName;
+    }
+
 }
