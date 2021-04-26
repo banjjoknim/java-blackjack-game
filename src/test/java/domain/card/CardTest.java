5181464c02aadcc8051171913ed7b71c906863df
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-26 23:19:10 +0900

    test : Card 의 Type 이 ACE 인지 판단하는 기능 테스트 추가

--- src/test/java/domain/card/CardTest.java
+++ src/test/java/domain/card/CardTest.java
@@ -2,7 +2,11 @@ package domain.card;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -29,5 +33,33 @@ class CardTest {
             );
         }
     }
+
+    @DisplayName("Card 의 Type 이 ACE 인지 판단하는 기능을 테스트한다.")
+    @ParameterizedTest
+    @MethodSource("provideCardAndExpectedResult")
+    void isAceTest(Card card, boolean expectedResult) {
+        // given
+
+        // when
+        boolean isAce = card.isAce();
+
+        // then
+        assertThat(isAce).isEqualTo(expectedResult);
+
+    }
+
+    private static Stream<Arguments> provideCardAndExpectedResult() {
+        return Stream.of(
+                Arguments.of(new Card(Symbol.SPADE, Type.ACE), true),
+                Arguments.of(new Card(Symbol.HEART, Type.ACE), true),
+                Arguments.of(new Card(Symbol.DIAMOND, Type.ACE), true),
+                Arguments.of(new Card(Symbol.CLUB, Type.ACE), true),
+                Arguments.of(new Card(Symbol.DIAMOND, Type.KING), false),
+                Arguments.of(new Card(Symbol.DIAMOND, Type.QUEEN), false),
+                Arguments.of(new Card(Symbol.DIAMOND, Type.JACK), false),
+                Arguments.of(new Card(Symbol.DIAMOND, Type.TEN), false)
+        );
+    }
+
 
 }
