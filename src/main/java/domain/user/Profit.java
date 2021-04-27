2c79c4713c006b59506886ca735b28a841a51713
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-28 00:36:08 +0900

    feat : Profit 클래스 추가

---
+++ src/main/java/domain/user/Profit.java
@@ -0,0 +1,13 @@
+package domain.user;
+
+import java.math.BigDecimal;
+
+public class Profit {
+
+    private BigDecimal amount;
+
+    public Profit(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+}
