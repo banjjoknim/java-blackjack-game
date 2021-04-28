8890423507f6b8e7970e8f1da86917be4d8e9e40
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-29 03:11:58 +0900

    style : 불필요한 import문 제거 및 변경

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class PlayerTest {
 
