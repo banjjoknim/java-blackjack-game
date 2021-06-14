3689c2e6610972bd6bfc78afdf8ba52d7d5ed0bc
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-15 02:59:37 +0900

    feat(User) : 유저의 카드로 총 점수를 계산하는 기능 추가

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -1,15 +1,39 @@
 package domain.user;
 
 import domain.card.Card;
+import domain.card.Type;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public abstract class User {
+    private static final int BLACKJACK = 21;
+    private static final int ACE_IS_ELEVEN = 10;
 
     private final List<Card> cards = new ArrayList<>();
 
     public void addCard(Card card) {
         cards.add(card);
+    }
+
+    public int calculateScore() {
+        int sum = cards.stream()
+                .map(Card::getType)
+                .mapToInt(Type::getScore)
+                .sum();
+        if (sum + ACE_IS_ELEVEN <= BLACKJACK && hasAceCard()) {
+            sum = sum + ACE_IS_ELEVEN;
+        }
+        return sum;
+    }
+
+    private boolean hasAceCard() {
+        return cards.stream()
+                .anyMatch(Card::isAce);
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
     }
 }
