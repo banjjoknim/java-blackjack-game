2a44d125a179828624396bd9dfebbb4366cdf38f
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-15 00:59:51 +0900

    feat : Type 추가

---
+++ src/main/java/domain/card/Type.java
@@ -0,0 +1,33 @@
+package domain.card;
+
+public enum Type {
+    KING("K", 10),
+    QUEEN("Q", 10),
+    JACK("J", 10),
+    TEN("10", 10),
+    NINE("9", 9),
+    EIGHT("8", 8),
+    SEVEN("7", 7),
+    SIX("6", 6),
+    FIVE("5", 5),
+    FOUR("4", 4),
+    THREE("3", 3),
+    TWO("2", 2),
+    ACE("A", 1);
+
+    private String name;
+    private int number;
+
+    Type(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
