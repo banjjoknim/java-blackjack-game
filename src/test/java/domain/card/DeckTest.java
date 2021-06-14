31726ca75347d271ef33750872998c9cfaa22ad0
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-15 03:51:09 +0900

    test(DeckTest) : 유저에게 카드를 나눠주는 기능 테스트 추가

---
+++ src/test/java/domain/card/DeckTest.java
@@ -0,0 +1,33 @@
+package domain.card;
+
+import domain.user.BettingMoney;
+import domain.user.Name;
+import domain.user.Player;
+import domain.user.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @DisplayName("유저에게 카드를 나눠주는 기능을 테스트한다.")
+    @Test
+    void 유저에게_카드를_나눠준다() {
+        // given
+        Deck deck = new Deck();
+
+        // when
+        User user = new Player(new Name("name"), new BettingMoney(new BigDecimal("1000")));
+        deck.distributeCard(user);
+
+        // then
+        assertAll(
+                ()->assertThat(user.getHand().getCards()).hasSize(1),
+                ()->assertThat(deck.getCards()).hasSize(51)
+        );
+    }
+}
