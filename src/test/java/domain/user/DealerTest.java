5414a5e770ec58dc2e3d6aa3f118f5b9f332b38e
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-16 01:49:38 +0900

    test(DealerTest) : 딜러가 대기 상태인지 검증하는 기능 테스트 추가

--- src/test/java/domain/user/DealerTest.java
+++ src/test/java/domain/user/DealerTest.java
@@ -1,7 +1,17 @@
 package domain.user;
 
+import domain.card.Card;
+import domain.card.Symbol;
+import domain.card.Type;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -20,6 +30,32 @@ class DealerTest {
         assertAll(
                 () -> assertDoesNotThrow(Dealer::new),
                 () -> assertThat(new Dealer().getHand().getCards()).hasSize(0)
+        );
+    }
+
+    @DisplayName("딜러가 대기 상태인지 검증하는 기능을 테스트한다.")
+    @ParameterizedTest
+    @MethodSource("provideCardsAndExpected")
+    void 딜러가_대기_상태인지_검증한다(List<Card> cards, boolean expected) {
+        // given
+        Dealer dealer = new Dealer();
+        cards.forEach(dealer::draw);
+
+        // when
+        boolean isWait = dealer.isWait();
+
+        // then
+        assertThat(isWait).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideCardsAndExpected() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.ACE)), false),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.QUEEN)), false),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.FIVE)), true),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.QUEEN),
+                        new Card(Symbol.SPADE, Type.THREE)), false),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.SEVEN)), false)
         );
     }
 }
