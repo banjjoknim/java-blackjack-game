fcbdb1c6336d570d1a5cfbd07e4f814110d9ded4
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-16 03:13:51 +0900

    test(UsersTest) : 유저들 중에서 대기중인 플레이어의 존재 유무 판단 기능, 대기중인 플레이어를 찾는 기능 테스트 추가

--- src/test/java/domain/user/UsersTest.java
+++ src/test/java/domain/user/UsersTest.java
@@ -1,12 +1,16 @@
 package domain.user;
 
+import domain.card.Card;
 import domain.card.Deck;
+import domain.card.Symbol;
+import domain.card.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -65,10 +69,74 @@ class UsersTest {
         // then
         List<User> list = users.getUsers();
         assertAll(
-                ()-> assertThat(list.get(0).getHand().getCards()).hasSize(1),
-                ()-> assertThat(list.get(1).getHand().getCards()).hasSize(1),
-                ()-> assertThat(list.get(2).getHand().getCards()).hasSize(1)
+                () -> assertThat(list.get(0).getHand().getCards()).hasSize(1),
+                () -> assertThat(list.get(1).getHand().getCards()).hasSize(1),
+                () -> assertThat(list.get(2).getHand().getCards()).hasSize(1)
         );
     }
 
+    @DisplayName("유저들 중에서 대기중인 플레이어가 존재할 때 대기중인 플레이어의 존재 유무를 판단하는 기능을 테스트한다.")
+    @Test
+    void 대기중인_플레이어가_존재한다() {
+        // given
+        userList.add(new Dealer());
+        Users users = new Users(userList);
+
+        // when
+        boolean hasWaitingPlayer = users.hasWaitingPlayer();
+
+        // then
+        assertThat(hasWaitingPlayer).isTrue();
+    }
+
+    @DisplayName("유저들 중에서 대기중인 플레이어가 존재하지 않을 때 대기중인 플레이어의 존재 유무를 판단하는 기능을 테스트한다.")
+    @Test
+    void 대기중인_플레이어가_존재하지_않는다() {
+        // given
+        userList.add(new Dealer());
+        List<Card> cards = Arrays.asList(new Card(Symbol.SPADE, Type.ACE), new Card(Symbol.SPADE, Type.KING));
+        User user1 = userList.get(0);
+        User user2 = userList.get(1);
+        cards.forEach(user1::draw);
+        cards.forEach(user2::draw);
+        Users users = new Users(userList);
+
+        // when
+        boolean hasWaitingPlayer = users.hasWaitingPlayer();
+
+        // then
+        assertThat(hasWaitingPlayer).isFalse();
+    }
+
+    @DisplayName("대기중인 플레이어가 존재할 때 유저들 중에서 대기중인 플레이어를 찾는 기능을 테스트한다.")
+    @Test
+    void 대기중인_플레이어가_존재할_때_대기중인_플레이어를_찾는다() {
+        // given
+        userList.add(new Dealer());
+        Users users = new Users(userList);
+
+        // when
+
+        // then
+        assertDoesNotThrow(users::findWaitingPlayer);
+    }
+
+    @DisplayName("대기중인 플레이어가 존재하지 않을 때 유저들 중에서 대기중인 플레이어를 찾는 기능을 테스트한다.")
+    @Test
+    void 대기중인_플레이어가_존재하지_않을_때_대기중인_플레이어를_찾으면_예외가_발생한다() {
+        // given
+        userList.add(new Dealer());
+        List<Card> cards = Arrays.asList(new Card(Symbol.SPADE, Type.ACE), new Card(Symbol.SPADE, Type.KING));
+        User user1 = userList.get(0);
+        User user2 = userList.get(1);
+        cards.forEach(user1::draw);
+        cards.forEach(user2::draw);
+        Users users = new Users(userList);
+
+        // when
+
+        // then
+        assertThatThrownBy(users::findWaitingPlayer)
+                .isInstanceOf(IllegalStateException.class);
+    }
 }
