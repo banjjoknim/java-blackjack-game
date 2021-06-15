516d55d39ee348b953ba8d2c2e062abc4ed60381
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-15 22:03:01 +0900

    refactor(State) : Ended 추가 및 Blackjack, Bust, Stay가 Ended를 상속받도록 변경

--- src/main/java/domain/user/state/Bust.java
+++ src/main/java/domain/user/state/Bust.java
@@ -1,8 +1,4 @@
 package domain.user.state;
 
-public class Bust extends State {
-    @Override
-    public State findNextState(int score, boolean isInitialCards) {
-        throw new IllegalStateException("차례가 끝났습니다.");
-    }
+public class Bust extends Ended {
 }
