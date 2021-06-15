5c1f8f7d17b499b2976aee149db610199a18d8db
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-15 14:55:26 +0900

    test(UsersTest) : 유저들이 각각 덱으로부터 카드를 나눠받는 기능 테스트 추가

--- src/test/java/domain/user/UsersTest.java
+++ src/test/java/domain/user/UsersTest.java
@@ -1,5 +1,6 @@
 package domain.user;
 
+import domain.card.Deck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class UsersTest {
@@ -43,12 +45,16 @@ class UsersTest {
         // when
 
         // then
-        assertDoesNotThrow(() -> new Users(userList));
+        assertAll(
+                () -> assertDoesNotThrow(() -> new Users(userList)),
+                () -> assertThat(new Users(userList).getUsers()).hasSize(3)
+        );
     }
 
     @DisplayName("유저들 중에서 딜러를 찾는 기능을 테스트한다.")
     @Test
     void 유저들_중에서_딜러를_찾는다() {
+        // given
         userList.add(new Dealer());
 
         // when
@@ -57,6 +63,26 @@ class UsersTest {
 
         // then
         assertThat(user).isInstanceOf(Dealer.class);
+    }
+
+    @DisplayName("딜러와 유저들이 각각 덱으로부터 카드를 나눠받는 기능을 테스트한다.")
+    @Test
+    void 딜러와_유저들이_각각_카드를_받는다() {
+        // given
+        userList.add(new Dealer());
+        Deck deck = new Deck();
+
+        // when
+        Users users = new Users(userList);
+        users.receiveCards(deck);
+
+        // then
+        List<User> list = users.getUsers();
+        assertAll(
+                ()-> assertThat(list.get(0).getHand().getCards()).hasSize(1),
+                ()-> assertThat(list.get(1).getHand().getCards()).hasSize(1),
+                ()-> assertThat(list.get(2).getHand().getCards()).hasSize(1)
+        );
     }
 
 }
