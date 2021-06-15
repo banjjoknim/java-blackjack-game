b0ee35b7fe834b4d174718a17a56209a3e695263
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-16 04:42:00 +0900

    test(BlackjackGameTest) : 딜러의 차례 여부 판단 기능 테스트 추가

--- src/test/java/domain/blackjack/BlackjackGameTest.java
+++ src/test/java/domain/blackjack/BlackjackGameTest.java
@@ -8,11 +8,15 @@ import domain.user.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -136,5 +140,77 @@ class BlackjackGameTest {
 
         // then
         assertThat(player.getHand().getCards()).hasSize(1);
+    }
+
+    @DisplayName("대기중인 플레이어가 없고 딜러의 점수가 17보다 클 때, 딜러의 차례인지 여부 판단을 테스트한다.")
+    @ParameterizedTest
+    @MethodSource("provideCardsOfOverRuleScore")
+    void 대기중인_플레이어가_없고_딜러의_점수가_룰로_지정된_점수보다_크면_딜러의_차례가_아니다(List<Card> cards) {
+        // given
+        List<Card> cardList = Arrays.asList(new Card(Symbol.SPADE, Type.ACE), new Card(Symbol.HEART, Type.KING));
+        User user1 = userList.get(0);
+        User user2 = userList.get(1);
+        cardList.forEach(user1::draw);
+        cardList.forEach(user2::draw);
+        Dealer dealer = users.findDealer();
+        cards.forEach(dealer::draw);
+        BlackjackGame blackjackGame = new BlackjackGame(users, deck);
+
+        // when
+        boolean isDealerPhase = blackjackGame.isDealerPhase();
+
+        // then
+        assertThat(isDealerPhase).isFalse();
+    }
+
+    private static Stream<Arguments> provideCardsOfOverRuleScore() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.ACE))),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.SEVEN),
+                        new Card(Symbol.HEART, Type.FOUR))),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.QUEEN))),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.SEVEN)))
+        );
+    }
+
+    @DisplayName("대기중인 플레이어가 없고 딜러의 점수가 17보다 작을 때, 딜러의 차례인지 여부 판단을 테스트한다.")
+    @ParameterizedTest
+    @MethodSource("provideCardsOfUnderRuleScore")
+    void 대기중인_플레이어가_없고_딜러의_점수가_룰로_지정된_점수보다_작으면_딜러의_차례이다(List<Card> cards) {
+        // given
+        List<Card> cardList = Arrays.asList(new Card(Symbol.SPADE, Type.ACE), new Card(Symbol.HEART, Type.KING));
+        User user1 = userList.get(0);
+        User user2 = userList.get(1);
+        cardList.forEach(user1::draw);
+        cardList.forEach(user2::draw);
+        Dealer dealer = users.findDealer();
+        cards.forEach(dealer::draw);
+        BlackjackGame blackjackGame = new BlackjackGame(users, deck);
+
+        // when
+        boolean isDealerPhase = blackjackGame.isDealerPhase();
+
+        // then
+        assertThat(isDealerPhase).isTrue();
+    }
+
+    private static Stream<Arguments> provideCardsOfUnderRuleScore() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.SIX))),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.KING), new Card(Symbol.SPADE, Type.FIVE))),
+                Arguments.of(Arrays.asList(new Card(Symbol.SPADE, Type.FIVE), new Card(Symbol.SPADE, Type.ACE))));
+    }
+
+    @DisplayName("대기중인 플레이어가 있을 때, 딜러가 카드를 뽑을 차례인지 여부 판단을 테스트한다.")
+    @Test
+    void 대기중인_플레이어가_있으면_딜러의_점수와_관계_없이_딜러의_차례가_아니다() {
+        // given
+        BlackjackGame blackjackGame = new BlackjackGame(users, deck);
+
+        // when
+        boolean isDealerPhase = blackjackGame.isDealerPhase();
+
+        // then
+        assertThat(isDealerPhase).isFalse();
     }
 }
