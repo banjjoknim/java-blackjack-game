4b4e8d76cf27d7baf7f8c3c510f52e8d8dff8521
WIN10-01\UserK <zhfxmtkachdt@daum.net>
2021-06-23 18:23:00 +0900

    refactor(InputView) : 게임에 참여할 사람의 이름을 입력하는 기능 일부 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.util.stream.Collectors.toList;
+
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
     private static final String SEPARATOR = ",";
@@ -19,7 +21,9 @@ public class InputView {
 
     public static List<String> inputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요(쉼표 기준으로 분리).");
-        return Arrays.asList(scanner.nextLine().split(SEPARATOR));
+        return Arrays.stream(scanner.nextLine().split(SEPARATOR))
+                .map(String::trim)
+                .collect(toList());
     }
 
     public static BigDecimal inputBettingMoneyAmount(Name name) {
