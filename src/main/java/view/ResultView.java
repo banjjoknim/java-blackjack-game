ba870406b7a1f91c911a3a8cdbdb248f2c24ae34
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-23 23:15:10 +0900

    refactor(ResultView) : 초기 카드를 나눠준 후, 딜러와 플레이어의 카드 결과를 출력하는 기능 수정

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -4,37 +4,47 @@ import domain.card.Card;
 import domain.user.Dealer;
 import domain.user.Player;
 
+import java.util.List;
+
 import static java.util.stream.Collectors.joining;
 
 public class ResultView {
     private static final int FIRST = 0;
-    private static final String CARD_SEPARATOR = ", ";
+    private static final String CARD_INFORMATION_SEPARATOR = ", ";
 
     private ResultView() {
     }
 
-    public static void printDealerCards(Dealer dealer) {
-        StringBuilder messageBuilder = new StringBuilder("딜러 카드 : ");
+    public static void printDealPhaseResult(Dealer dealer, List<Player> players) {
+        StringBuilder resultBuilder = new StringBuilder();
+        appendDealPhaseResultOfDealer(resultBuilder, dealer);
+        appendDealPhaseResultOfPlayers(resultBuilder, players);
+        System.out.println(resultBuilder);
+    }
+
+    private static void appendDealPhaseResultOfDealer(StringBuilder resultBuilder, Dealer dealer) {
         Card card = dealer.getState().getCards().get(FIRST);
-        messageBuilder.append(buildMessage(card));
-        System.out.println(messageBuilder);
+        resultBuilder.append("딜러 카드 : ");
+        resultBuilder.append(buildCardInformation(card));
     }
 
-    public static void printPlayerCards(Player player) {
-        StringBuilder messageBuilder = new StringBuilder();
-        String playerMessage = player.getName().getName() + " 카드 : ";
-        String cardsMessage = player.getState().getCards().stream()
-                .map(ResultView::buildMessage)
-                .collect(joining(CARD_SEPARATOR));
-        messageBuilder.append(playerMessage);
-        messageBuilder.append(cardsMessage);
-        System.out.println(messageBuilder);
+    private static void appendDealPhaseResultOfPlayers(StringBuilder resultBuilder, List<Player> players) {
+        players.forEach(player -> appendDealPhaseResultOfPlayer(resultBuilder, player));
     }
 
-    private static StringBuilder buildMessage(Card card) {
-        StringBuilder messageBuilder = new StringBuilder();
-        messageBuilder.append(card.getType().getName());
-        messageBuilder.append(card.getSymbol().getName());
-        return messageBuilder;
+    private static void appendDealPhaseResultOfPlayer(StringBuilder resultBuilder, Player player) {
+        resultBuilder.append("\n");
+        resultBuilder.append(player.getName().getName() + " 카드 : ");
+        String playerCardsResult = player.getState().getCards().stream()
+                .map(ResultView::buildCardInformation)
+                .collect(joining(CARD_INFORMATION_SEPARATOR));
+        resultBuilder.append(playerCardsResult);
+    }
+
+    private static StringBuilder buildCardInformation(Card card) {
+        StringBuilder informationBuilder = new StringBuilder();
+        informationBuilder.append(card.getType().getName());
+        informationBuilder.append(card.getSymbol().getName());
+        return informationBuilder;
     }
 }
