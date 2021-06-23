c6d44c5c724445fd895ca763dd3caeeb83a14b98
WIN10-01\UserK <zhfxmtkachdt@daum.net>
2021-06-23 17:44:57 +0900

    refactor(InputView) : 인스턴스화 하지 못하도록 private 기본 생성자 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,9 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
     private static final String SEPARATOR = ",";
 
+    private InputView() {
+    }
+
     public static List<String> inputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요(쉼표 기준으로 분리).");
         return Arrays.asList(scanner.nextLine().split(SEPARATOR));
