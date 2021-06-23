bde5e2ae79156c2c746037d12808e40e09c5ce23
WIN10-01\UserK <zhfxmtkachdt@daum.net>
2021-06-23 17:55:54 +0900

    feat(InputView) : 유저 배팅 금액을 입력하는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,11 @@
 package view;
 
+import domain.user.BettingMoney;
+import domain.user.Name;
+import domain.user.Player;
+import jdk.internal.util.xml.impl.Input;
+
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -14,5 +20,12 @@ public class InputView {
     public static List<String> inputNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요(쉼표 기준으로 분리).");
         return Arrays.asList(scanner.nextLine().split(SEPARATOR));
+    }
+
+    public static BigDecimal inputBettingMoneyAmount(Name name) {
+        StringBuilder messageBuilder = new StringBuilder(name.getName());
+        messageBuilder.append("의 배팅 금액은?");
+        System.out.println(messageBuilder);
+        return BigDecimal.valueOf(scanner.nextInt());
     }
 }
