cc8531e659ff5c155440603e4858bf5a578e3ecf
banjjoknim <zhfxmtkachdt@daum.net>
2021-06-25 03:26:32 +0900

    feat(Profit) : 수익을 더하는 기능 및 수익의 부호를 반전시키는 기능 추가

--- src/main/java/domain/user/Profit.java
+++ src/main/java/domain/user/Profit.java
@@ -4,11 +4,20 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Profit {
+    public static final Profit ZERO = new Profit(BigDecimal.ZERO);
 
     private BigDecimal amount;
 
     public Profit(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public Profit add(Profit profit) {
+        return new Profit(this.amount.add(profit.amount));
+    }
+
+    public Profit negate() {
+        return new Profit(amount.negate());
     }
 
     public BigDecimal getAmount() {
